51267ed02cf665dc89202daf67639d904545bfad
Viktor Szépe <viktor@szepe.net>
2022-11-12 09:44:15 +0000

    Next try

--- src/all.php
+++ src/all.php
@@ -3,4 +3,4 @@
 $configuration = require dirname(__DIR__) . '/example-config.php';
 
 $ghaWorkflow = yaml_parse_file($configuration['github_actions_workflow']);
-var_dump($ghaWorkflow);
+var_dump($ghaWorkflow['jobs']['run-all']['steps'][0]['with']['php-version']);
